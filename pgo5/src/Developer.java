e0bb06e476bb796d6fa701d5492e50d22bc31825
s30523 <162446714+s30523@users.noreply.github.com>
2024-05-08 04:11:21 +0200

    Developer.java

--- pgo5/src/Developer.java
+++ pgo5/src/Developer.java
@@ -1,20 +1,33 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Developer {
-    public String firstName;
-    public String lastName;
-    public String address;
-    public String email;
-    public String pesel;
-    public int YearOfEmployment;
+public class Developer extends Employee {
     public ArrayList<Technology>technologies;
 
    public Developer(String firstName, String lastName, String address, String email, String pesel, int yearOfEmployment){
+       super(firstName,lastName,adress,email,pesel,yearOfEmployment);
        this.firstName=firstName;
        this.lastName=lastName;
        this.address=address;
        this.email=email;
        this.pesel=pesel;
-       this.YearOfEmployment=yearOfEmployment;
+       this.yearOfEmployment=yearOfEmployment;
     }
+    public void addTechnology(Technology technology) {
+    }
+    public int calculateSalary() {
+        int sallary=super.calculateSalary();
+        LocalDate date=LocalDate.now();
+        int year = date.getYear();
+        int WorkingYeares = year - YeaOffEmployment;
+        for(int i=1; i<=WorkingYeares; i++){
+            sallary+=1000;
+
+            for(Technology technology: technologies){
+                sallary+=technology.bonus;
+            }
+        }
+        return sallary;
+    }
+    
 }
