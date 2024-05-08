b3a9eea6ba393a325124051975021ed126273117
s30523 <162446714+s30523@users.noreply.github.com>
2024-05-08 04:17:42 +0200

    Employee.java

--- pgo5/src/Employee.java
+++ pgo5/src/Employee.java
@@ -6,7 +6,22 @@ public class Employee {
     public String adress;
     public String email;
     public String pesel;
-    public int YearOfEmployment;
+    public int yearOfEmployment;
     public int salary;
     public ArrayList<Employee> employees;
+
+     public Employee(String firstName, String lastName, String address, String email, String pesel, int yearOfEmployment){
+       this.firstName=firstName;
+       this.lastName=lastName;
+       this.address=address;
+       this.email=email;
+       this.pesel=pesel;
+       this.yearOfEmployment=yearOfEmployment;
+}
+    public int calculateSalary() {
+        int sallary=3000;
+        return salary;
+
+    }
+
 }
