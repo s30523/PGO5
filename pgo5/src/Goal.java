e7412a1dda684976d3c77d2c0bdf34d22c8553c2
s30523 <162446714+s30523@users.noreply.github.com>
2024-05-08 04:20:06 +0200

    Goal.java

--- pgo5/src/Goal.java
+++ pgo5/src/Goal.java
@@ -1,11 +1,16 @@
+import java.time.LocalDate;
 public class Goal {
     public int year;
     public int month;
     public int day;
-    public Goal(int year, int month, int day){
+    public String name;
+    public int bonus;
+    public Goal(int year, int month, int day, String name, int bonus){
         this.year=year;
         this.month=month;
         this.day=day;
+        this.name=name;
+        this.bonus=bonus;
 
     }
 }
